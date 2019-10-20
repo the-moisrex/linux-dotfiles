d5b900040d08840b1678bb9aa9befac36a228c83
Mohammad Bahoosh <moisrex@protonmail.com>
2019-10-20 03:32:34 +0000

    adding python

---
+++ cpp-templates/py/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Module Defenition
+
+main:
+    calling the main thing
+"""
+
+def main():
+    """The Main function"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
