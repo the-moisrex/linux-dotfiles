f0af5ea1f023af52e53ba0b228d09dfcb6983047
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-01 14:50:06 +0000

    added PCH to boost template

--- code-templates/boost/main.cpp
+++ code-templates/boost/main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <boost/algorithm/string/join.hpp>
+#include "pch.h"
 
 using namespace std;
 
---
+++ code-templates/boost/pch.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string/join.hpp>
