8d3fa491c48bc2261c0b6d15509cf77848cc4901
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-06 00:17:42 +0000

    boost.asio added

---
+++ code-templates/boost.asio/main.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+using namespace boost;
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+auto main() -> int {
+
+  io_context io;
+
+
+  return 0;
+}
