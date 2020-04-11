5fc1a0f53fb86272ff345236c8695ea674b8cb22
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-11 07:09:14 -0400

    gtest code template added

---
+++ code-templates/gtest/main.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+using namespace std;
+
+TEST(Main, Init) {
+  EXPECT_TRUE(true);
+}
+
+
---
+++ code-templates/gtest/pch.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
