192746d0a2481f29a73de639c072c37535e779d5
Mohammad Bahoosh <moisrex@protonmail.com>
2020-09-13 08:52:31 -1000

    cpu added

--- code-templates/c.linux/tlpi_hdr.h
+++ code-templates/c.linux/tlpi_hdr.h
@@ -2,6 +2,7 @@
 #define TLPI_HDR_H /* Prevent accidental double inclusion */
 
 #include <sys/types.h> /* Type definitions used by many programs */
+#include <sys/stat.h>
 #include <stdio.h>     /* Standard I/O functions */
 #include <stdlib.h>    /* Prototypes of commonly used library functions,
                           plus EXIT_SUCCESS and EXIT_FAILURE constants */
