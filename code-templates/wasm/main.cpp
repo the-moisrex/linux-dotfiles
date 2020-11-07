bb0e0878a57099f85330696378043ee417879307
Mohammad Bahoosh <moisrex@protonmail.com>
2020-11-06 16:41:07 -1000

    more wasm

--- code-templates/wasm/main.cpp
+++ code-templates/wasm/main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
+#include <emscripten.h>
+
+#include <string_view>
 using namespace std;
+
+EM_JS(void, js_console_log, (char const* data, std::size_t _size), {
+    const buf = new Uint8Array(wasmMemory.buffer, data, _size);
+    console.log(new TextDecoder("utf-8").decode(buf));
+  });
+
+struct console {
+  static void log(std::string_view str) {
+    js_console_log(str.data(), str.size());
+  }
+};
+
 
 
 int main() {
 
-  cout << "Hello world" << endl;
+  emscripten_async_run_script("console.log('Hello From run_script');", 2000);
+  console::log("JS inside C++");
 
   return 0;
 }
+
---
+++ code-templates/wasm/public/post.js
@@ -0,0 +1,3 @@
+// This file will be run when the wasm is ready
+
+console.log("JS is ready as well.");
