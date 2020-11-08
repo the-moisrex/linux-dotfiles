fc5556cfcd298d72c2ebdf3e38a8e6ae770b60b2
Mohammad Bahoosh <moisrex@protonmail.com>
2020-11-07 18:11:55 -1000

    boost.beast

---
+++ code-templates/boost.beast/pch.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <memory>
+#include <cstdlib>
+#include <set>
+#include <deque>
+#include <list>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/bind.hpp>
