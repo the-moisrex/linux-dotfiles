7ec0702798bd5689c1a47f64ed8652792580bd5b
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-06-05 07:38:00 -0800

    unsudo nft

--- shell/aliases.fish
+++ shell/aliases.fish
@@ -162,5 +162,5 @@ function unsudo -d "un-sudo some commands"
     end
 end
 
-unsudo pacman systemctl ausearch reboot poweroff auditctl aureport dnstop nftop
+unsudo pacman systemctl ausearch reboot poweroff auditctl aureport dnstop nftop nft
 
