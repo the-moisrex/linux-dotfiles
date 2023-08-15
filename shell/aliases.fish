60a528ec79e972e99cbc44b4233caa9b6413484f
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-08-15 00:30:15 -0800

    snvim

--- shell/aliases.fish
+++ shell/aliases.fish
@@ -129,6 +129,7 @@ alias tmp="cd $(mktemp -d)"
 alias open="emacsclient -nc -s initd -a nvim"
 alias edit="emacsclient -nw -s initd -a nvim"
 alias svim="vim --clean" # Simple vim
+alias snvim="nvim --noplugin --clean" # Simple nvim
 alias proxychains="http_proxy=\"\" https_proxy=\"\" all_proxy=\"\" proxychains"
 alias c="xclip -selection clipboard"
 
