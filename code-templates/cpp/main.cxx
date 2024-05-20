55683cb32fecad82caea5207d8d0308e10628f08
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-05-20 13:43:11 -0700

    add more editor support and alias

--- code-templates/cpp/main.cxx
+++ code-templates/cpp/main.cxx
@@ -1,11 +1,15 @@
 import standard;
+
+#include <cstdint>
 #include <print>
 
 using namespace std;
 
+static constexpr uint8_t worlds = 7;
+
 int main() {
 
-  println("Hello all {} worlds.", 7);
+  println("Hello all {} worlds.", worlds);
 
   return 0;
 }
