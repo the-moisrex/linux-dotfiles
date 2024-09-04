dd54e663c4c32bb28195d5bee7833fab40b750b5
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-09-03 19:04:01 -1000

    nvim configs

---
+++ configs/nvim/after/plugin/lsp.lua
@@ -0,0 +1,76 @@
+---
+-- LSP configuration
+---
+local lsp_zero = require('lsp-zero')
+
+local lsp_attach = function(client, bufnr)
+  local opts = {buffer = bufnr}
+
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+-- These are just examples. Replace them with the language
+-- servers you have installed in your system
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup({})
+lspconfig.bashls.setup({})
+lspconfig.pylsp.setup({})
+lspconfig.rust_analyzer.setup({})
+lspconfig.ccls.setup({
+  init_options = {
+    compilationDatabaseDirectory = "build";
+    index = {
+      threads = 0;
+    };
+    clang = {
+      excludeArgs = { "-frounding-math"} ;
+    };
+  }
+})
+
+-- require('mason').setup({})
+-- require('mason-lspconfig').setup({
+--   -- Replace the language servers listed here
+--   -- with the ones you want to install
+--   ensure_installed = {'tsserver', 'rust_analyzer', 'eslint'},
+--   handlers = {
+--     function(server_name)
+--       require('lspconfig')[server_name].setup({})
+--     end,
+--   }
+-- })
+
+---
+-- Autocompletion setup
+---
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
+})
+
---
+++ configs/nvim/lua/sysman/remap.lua
@@ -0,0 +1,52 @@
+vim.g.mapleader = " "
+
+-- Explore
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- When highlighted, we can move the selected block up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Change the behaviour of J: don't move the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- vim.keymap.set("n", "Y", "yg$")
+
+-- Centering the page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Preserving the yanked string, but paste it without yanking anything
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Ynak into system's clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Disabling Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Tmux
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+
+-- Replace the thing under the cursor:
+vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+
