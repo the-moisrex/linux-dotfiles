511369f206f9a003a2d714808e858ec2bc5ec11b
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-09-07 04:36:57 -1000

    neovim NerdTree

---
+++ configs/nvim/after/plugin/nerdtree.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeToggle<CR>', {
+    noremap = true,
+    silent = true 
+})
+
+vim.api.nvim_set_keymap('n', '<leader>fs', ':NERDTreeToggle<CR>', {
+    noremap = true,
+    silent = true 
+})
+
+
