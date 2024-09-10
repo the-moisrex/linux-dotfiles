7c763c7d4615f7ae322b6aa453915051e2201c2d
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-09-10 03:29:00 -1000

    todo comments update

--- configs/nvim/after/plugin/todo-comments.lua
+++ configs/nvim/after/plugin/todo-comments.lua
@@ -32,7 +32,7 @@ require('todo-comments').setup({
     before = "fg", -- "fg" or "bg" or empty
     keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
     after = "fg", -- "fg" or "bg" or empty
-    pattern = [[.*<(KEYWORDS)\s*:?]], -- pattern or table of patterns, used for highlighting (vim regex)
+    pattern = [[\b.*<(KEYWORDS)\s*:?]], -- pattern or table of patterns, used for highlighting (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
     exclude = {}, -- list of file types to exclude highlighting
@@ -58,7 +58,7 @@ require('todo-comments').setup({
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):?]], -- ripgrep regex
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
     -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   },
 });
