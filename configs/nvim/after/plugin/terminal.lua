d93ff75c119f8df350d74e29a2de62375736a0c8
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2025-01-09 05:34:10 -1000

    fix terminal toggle bug

--- configs/nvim/after/plugin/terminal.lua
+++ configs/nvim/after/plugin/terminal.lua
@@ -1,18 +1,23 @@
 
 local function is_buffer_visible(buf_number)
-    -- Get all windows
-    local windows = vim.api.nvim_list_wins()
-    
-    -- Iterate through each window
-    for _, win in ipairs(windows) do
-        -- Get the buffer associated with the window
-        local win_buf = vim.api.nvim_win_get_buf(win)
-        if win_buf == buf_number then
-            return true  -- Buffer is visible in this window
-        end
-    end
-    
-    return false  -- Buffer is not visible in any window
+    -- Get a list of window IDs that show the specified buffer
+    local windows = vim.fn.win_findbuf(buf_number)
+    -- Check if the list is not empty
+    return #windows > 0
+end
+
+-- local function is_terminal_buffer(buf_number)
+--     -- Get the term_title variable for the buffer
+--     local term_title = vim.fn.getbufvar(buf_number, 'term_title')
+--     -- Check if term_title is set (not empty)
+--     return term_title ~= ''
+-- end
+
+local function is_terminal_buffer(buf_number)
+    -- Get the buffer type using getbufvar
+    local buftype = vim.fn.getbufvar(buf_number, "&buftype")
+    -- Check if the buffer type is 'terminal'
+    return buftype == 'terminal'
 end
 
 function auto_split()
@@ -27,13 +32,12 @@ function auto_split()
     end
 end
 
-
 function toggle_terminal()
     local buffers = vim.api.nvim_list_bufs()
     local terminal_buf = nil
 
     for _, buf in ipairs(buffers) do
-        if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+        if is_terminal_buffer(buf) then
             terminal_buf = buf
             break
         end
@@ -56,8 +60,10 @@ function toggle_terminal()
     end
 end
 
-vim.keymap.set('n', '<F12>', '<ESC><C-\\><C-n>:lua toggle_terminal()<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<F12>', '<ESC><C-\\><C-n>:lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F12>', toggle_terminal, { noremap = true, silent = true })
+vim.keymap.set('t', '<F12>', function() 
+    vim.api.nvim_win_hide(vim.api.nvim_get_current_win())
+end, { noremap = true, silent = true })
 
 
 -- Open Terminal
