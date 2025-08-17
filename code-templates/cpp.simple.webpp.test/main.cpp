e3da38928f24d67fde3079a76d346297481a6e49
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-08-16 16:39:57 -1000

    Web++ test

---
+++ code-templates/cpp.simple.webpp.test/main.cpp
@@ -0,0 +1,10 @@
+#include "webpp/webpp.hpp"
+#include "./common/tests_common_pch.hpp"
+
+using namespace webpp;
+using namespace webpp::stl;
+using namespace webpp::istl;
+
+TEST(WebppTest, Test1) {
+    EXPECT_TRUE(true);
+}
