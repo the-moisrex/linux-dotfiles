5c544516f14728a60a1ca3323caa5863953cef6c
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-09-28 18:25:07 -1000

    Cleanup

---
+++ cleanup/apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# GUI Apps
+trash "$share/ClipGrab"
+trash "$share/ark"
+trash "$share/app.hiddify.com"
+trash "$share/ghostwriter"
+trash "$share/drkonqi"
+
+# Terminal apps
+trash "$share/ranger"
+trash "$share/NuGet"
---
+++ cleanup/logs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+trash "$share/imhex/logs"
+trash "$share/imhex/backups"
+
+trash "$share/akondi/Akondi.error"
+trash "$share/akondi/Akondi.error.old"
+trash "$share/akondi_migration_agent"
