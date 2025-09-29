dab2d7431e17a42f8a532b87ae03b31e839ab813
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-09-28 18:49:19 -1000

    Logs

--- cleanup/logs.sh
+++ cleanup/logs.sh
@@ -6,3 +6,7 @@ trash "$share/imhex/backups"
 trash "$share/akondi/Akondi.error"
 trash "$share/akondi/Akondi.error.old"
 trash "$share/akondi_migration_agent"
+
+trash "/var/log/nginx/access.log"
+trash "/var/log/pacman.log"
+trash "/var/log/haskell-register.log"
