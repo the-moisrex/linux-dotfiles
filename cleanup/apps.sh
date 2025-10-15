ce73d01e5292b811911f72e1c6199df8d269d36d
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-10-15 09:39:53 -1000

    Cleanup update

--- cleanup/apps.sh
+++ cleanup/apps.sh
@@ -6,7 +6,9 @@ trash "$share/ark"
 trash "$share/app.hiddify.com"
 trash "$share/ghostwriter"
 trash "$share/drkonqi"
+trash "$share/okular"
 
 # Terminal apps
 trash "$share/ranger"
 trash "$share/NuGet"
+trash "$share/man" # User Man Pages
--- cleanup/kde.sh
+++ cleanup/kde.sh
@@ -6,12 +6,28 @@ trash "$share/konsole"
 trash "$share/kontact"
 trash "$share/kpeople"
 trash "$share/kpeoplevcard"
+trash "$share/recently-used.xbel"
+trash "$share/kcookiejar"
 
 # Less used apps
-trash "$share/krita"
 trash "$share/kget"
 trash "$share/kalgebra"
 trash "$share/DisplayCAL"
+trash "$share/KDE/angelfish"
+trash "$share/KDE/audiotube"
+trash "$share/KDE/kasts"
+trash "$share/KDE/neochat"
+trash "$share/KDE/telly-skout"
+trash "$share/telly-skout"
+trash "$share/KDE/alligator"
+trash "$share/calibre-ebook.com"
+trash "$share/baloo"
+
+# Krita
+trash "$share/krita"
+trash "$share/krita.log"
+trash "$share/krita-sysinfo.log"
+trash "$share/kcachegrind"
 
 # KDE Stuff
 trash "$share/plasma_notes"
