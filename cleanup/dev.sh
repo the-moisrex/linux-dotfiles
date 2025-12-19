f79ad5caf31a828f986b928570c750bd3dd7bcd4
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-12-19 06:27:58 -1000

    Remove a bunch more

--- cleanup/dev.sh
+++ cleanup/dev.sh
@@ -6,3 +6,17 @@ trash "$share/devhelp"
 trash "$share/meld"
 trash "$share/GitKrakenCLI"
 trash "$share/DBeaverData"
+
+for file in $(find ~/codeshells/ -type d -name "build" -or -type d -name "build-clang" -or -type d -name "cmake-build-*" -or -type d -name ".venv" -or -type d -name "node_modules" 2>/dev/null); do
+    trash "$file"
+done
+
+
+trash "$HOME/.eclipse"
+trash "$HOME/.duckdb"
+trash "$HOME/.duckdb_history"
+trash "$HOME/.rest-client" # VSCode Extension
+trash "$HOME/.nuget"
+trash "$HOME/.lldb"
+trash "$HOME/.dotnet"
+trash "$HOME/.gk" # GitKraken
--- cleanup/logs.sh
+++ cleanup/logs.sh
@@ -10,3 +10,20 @@ trash "$share/akondi_migration_agent"
 trash "/var/log/nginx/access.log"
 trash "/var/log/pacman.log"
 trash "/var/log/haskell-register.log"
+
+trash "$HOME/.wget-hsts"
+trash "$HOME/.cargo"
+trash "$HOME/.npm"
+trash "$HOME/.conan2"
+trash "$HOME/.parallel"
+trash "$HOME/.icons"
+trash "$HOME/.var"
+trash "$HOME/.fontconfig"
+trash "$HOME/.java" # contains font configs, I think
+trash "$HOME/.swt" # extracted native libraries (.so files) which are essentially a cache
+trash "$HOME/.ipython"
+trash "$HOME/.skiko" # cache for unpacked native libraries for Jetbrains
+trash "$HOME/.shutter"
+trash "$HOME/.fltk"
+trash "$HOME/.designer"
+trash "$HOME/.codex/log"
