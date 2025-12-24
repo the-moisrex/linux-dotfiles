af8f3a7bfb91302fdc405eb8bce8a9a5b79551aa
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-12-24 07:32:35 -1000

    Disable sound, it prevents exit command from being run

--- shell/config.fish
+++ shell/config.fish
@@ -1,4 +1,3 @@
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
@@ -16,10 +15,10 @@ if status is-interactive
 
     # set -gx TERM=alacritty
     # set -gx TERM=kitty
-    set -gx EDITOR "nvim"
+    set -gx EDITOR nvim
     set -gx PATH ".:$HOME/.bin:$cmddir/bin:$cmddir/firewall:$HOME/.lmstudio/bin:$PATH:$HOME/.local/bin:$JAVA_HOME/bin:$HOME/.cargo/bin:$HOME/Android/Sdk/platform-tools:$HOME/Android/Sdk/emulator"
     set -gx CPM_SOURCE_CACHE "$HOME/.cache/CPM"
-    set -gx HISTSIZE "100000000"
+    set -gx HISTSIZE 100000000
     set -gx SAVEHIST "$HISTSIZE"
     # set -gx XDG_DATA_DIRS "$HOME/.nix-profile/share:$XDG_DATA_DIRS"
     # set -gx LD_LIBRARY_PATH "." "$LD_LIBRARY_PATH"
@@ -36,39 +35,39 @@ if status is-interactive
     # end
 
     # JAVA
-    set -gx JAVA_HOME "/usr/lib/jvm/java-20-openjdk"
+    set -gx JAVA_HOME /usr/lib/jvm/java-20-openjdk
     if [ ! -d "$JAVA_HOME" ]
         set -gx JAVA_HOME /usr/lib/jvm/default
     end
 
     # VCPkg (pacman -S vcpkg):
-    set -gx VCPKG_ROOT "/opt/vcpkg"
+    set -gx VCPKG_ROOT /opt/vcpkg
     if [ ! -d "$VCPKG_ROOT" ]
         set --erase VCPKG_ROOT
     end
 
     function conda -d 'lazy initialize conda'
-      functions --erase conda
-      eval /opt/miniconda3/bin/conda "shell.fish" "hook" | source
-      # There's some opportunity to use `psub` but I don't really understand it.
-      conda $argv
+        functions --erase conda
+        eval /opt/miniconda3/bin/conda "shell.fish" hook | source
+        # There's some opportunity to use `psub` but I don't really understand it.
+        conda $argv
     end
 
     # Play a sound on indicating the status of the last command
-    if play_pipe_sound --is-possible;
-        function precmd --on-event fish_postexec;
-            set p_status "$pipestatus"
-            echo -ne (set_color yellow)► [ "$p_status" ]
-            setsid play_pipe_sound $p_status >/dev/null 2>&1 </dev/null &
-        end
-    end
+    # if play_pipe_sound --is-possible;
+    #     function precmd --on-event fish_postexec;
+    #         set p_status "$pipestatus"
+    #         echo -ne (set_color yellow)► [ "$p_status" ]
+    #         setsid play_pipe_sound $p_status >/dev/null 2>&1 </dev/null &
+    #     end
+    # end
 
+    source $HOME/.config/fish/aliases.fish
 
-    source $HOME/.config/fish/aliases.fish;
-    source $HOME/.config/fish/completions/completions.fish;
+    source $HOME/.config/fish/completions/completions.fish
 
-    if [ -f "$HOME/.fishrc" ];
-        source $HOME/.fishrc;
+    if [ -f "$HOME/.fishrc" ]
+        source $HOME/.fishrc
     end
 
     # >>> conda initialize >>>
@@ -78,12 +77,8 @@ if status is-interactive
     # end
     # <<< conda initialize <<<
 
-
     source $HOME/.config/fish/cmd_timer.fish
 end
-
-
-
 
 # function try -d "try a command until it works."
 #     while ! $argv
@@ -97,4 +92,3 @@ end
 #         sleep 1s; # So we can cancel
 #     end
 # end
-
