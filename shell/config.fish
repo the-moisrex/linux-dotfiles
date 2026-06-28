c8f7a1d48b2cfc979df132904c99b5278b0970bf
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-06-27 14:21:36 -1000

    zoxide

--- shell/config.fish
+++ shell/config.fish
@@ -8,7 +8,7 @@ function save_last_dir --on-variable PWD
     if not test -d "$FISH_CACHE_DIR"
         mkdir -p "$FISH_CACHE_DIR"
     end
-    echo $PWD > "$LAST_DIR_FILE"
+    echo $PWD >"$LAST_DIR_FILE"
 end
 
 if status is-interactive
@@ -55,9 +55,7 @@ if status is-interactive
     set -gx RSPEC_COLOR 1 # Ruby
     set -gx BUILDKIT_COLORS 'run=green:info=cyan:warning=yellow:error=red' # Docker BuildKit
     set -gx GH_FORCE_TTY 100% # GitHub CLI
-    set -gx TF_CLI_ARGS "-color" # Terraform
-
-
+    set -gx TF_CLI_ARGS -color # Terraform
 
     # There are conflicts for "clang-format" for example
     # if [ -d /opt/depot_tools ];
@@ -111,7 +109,7 @@ if status is-interactive
     source $HOME/.config/fish/cmd_timer.fish
 
     function _open_pdf
-        echo "evince $argv"  # or your PDF viewer
+        echo "evince $argv" # or your PDF viewer
     end
     function _open_editor
         echo "$EDITOR $argv"
@@ -119,20 +117,18 @@ if status is-interactive
     function _open_editor
         echo "jless $argv"
     end
-    abbr --add 'pdf' --regex '\S*\.pdf$' --function _open_pdf
-    abbr --add 'nvim-text-files' --regex '\S*\.(md|cpp|hpp|cxx|ixx|h|txt)$' --function _open_editor
-    abbr --add 'json' --regex '\S*\.(json|yml|yaml)$' --function _open_editor
+    abbr --add pdf --regex '\S*\.pdf$' --function _open_pdf
+    abbr --add nvim-text-files --regex '\S*\.(md|cpp|hpp|cxx|ixx|h|txt)$' --function _open_editor
+    abbr --add json --regex '\S*\.(json|yml|yaml)$' --function _open_editor
 
     abbr -a "c." --position anywhere --set-cursor "c.p | % | c.c" # Paste, Modify, Copy
-    abbr -a "issue" --set-cursor "gh issue view --comments % | clean.privacy | c.c"
+    abbr -a issue --set-cursor "gh issue view --comments % | clean.privacy | c.c"
     abbr for-dirs --set-cursor=! "$(string join \n -- 'for dir in */' 'cd $dir' '!' 'cd ..' 'end')"
     abbr -a pro prompt
 
     abbr -a "yt.save" "c.p | yt.links >> ~/YouTube/tmp/links.txt; exit"
     abbr -a "yt.dl" "c.p | yt.links | xargs download --cookies-from-browser firefox 1440p; exit"
     abbr -a hx helix
-
-
 
     # On startup, read the file and cd into it
     # Only cd if we are starting in the home directory
@@ -145,6 +141,7 @@ if status is-interactive
         end
     end
 
+    zoxide init fish | source
 end
 
 # function try -d "try a command until it works."
